957f9f04c5ee2edfcdda025ca8085d9d7577f609
Agha Ali Abbas <agha.ali@codingpixel.com>
2020-05-16 20:41:41 +0500

    Updates

---
+++ precache-manifest.6faef1a2acb07fd33f79b814573843d9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2a228d7f1ad9c86cb3a54e6a1a2e52",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d1d26cf6a8847fa3e6c2",
+    "url": "/portfolio/static/css/main.49a12962.chunk.css"
+  },
+  {
+    "revision": "530d942a1aeb5d9a52e2",
+    "url": "/portfolio/static/js/2.f0a24b9d.chunk.js"
+  },
+  {
+    "revision": "d1d26cf6a8847fa3e6c2",
+    "url": "/portfolio/static/js/main.74678dd5.chunk.js"
+  },
+  {
+    "revision": "188e9364f63da17af3bc",
+    "url": "/portfolio/static/js/runtime-main.a22e96b3.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/portfolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/portfolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
